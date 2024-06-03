bd91d69a807745a132db64e9784f624e9751454a
FeesTheFox <feesblock@gmail.com>
2024-06-03 16:48:22 +0300

    Now you can change the dogs to the cats by using switch (if your role is super user)

--- app/src/main/java/com/example/petlocator/ClickActivity.java
+++ app/src/main/java/com/example/petlocator/ClickActivity.java
@@ -8,12 +8,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.petlocator.databinding.ActivityClickBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
@@ -32,22 +34,27 @@ public class ClickActivity extends AppCompatActivity {
     private int clicks = 0;
     private MediaPlayer mediaPlayer;
     private MediaPlayer mediaPlayer1;
+    private MediaPlayer mediaPlayer2;
     private Animation rotateAnimation;
 
     private FirebaseDatabase firebaseDatabase;
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
     private String userId;
+    ActivityClickBinding binding;
 
     private ImageView imageView;
     private boolean[] isReached = {false, false, false, false, false};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_click);
+        binding = com.example.petlocator.databinding.ActivityClickBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         mediaPlayer = MediaPlayer.create(this, R.raw.bark);
         mediaPlayer1 = MediaPlayer.create(this, R.raw.yippie);
+        mediaPlayer2 = MediaPlayer.create(this, R.raw.meow);
 
         rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_animation);
 
@@ -56,9 +63,61 @@ public class ClickActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
 
+        binding.catSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Сохраняем значение переключателя в базу данных Firebase
+                HashMap<String, Object> data = new HashMap<>();
+                data.put("catSwitch", isChecked);
+                firebaseDatabase.getReference("Users").child(userId).updateChildren(data)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // Отображаем количество кликов на экране
+                                setImageAndClicksText();
+                            }
+                        });
+            }
+        });
+
         if (firebaseUser != null) {
             // Получаем уникальный идентификатор пользователя
             userId = firebaseUser.getUid();
+
+            DatabaseReference userRef = firebaseDatabase.getReference("Users").child(userId);
+
+            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String role = snapshot.child("role").getValue(String.class);
+                        if ("Супер пользователь".equals(role)) {
+                            binding.catSwitch.setVisibility(View.VISIBLE);
+                        } else {
+                            binding.catSwitch.setVisibility(View.GONE);
+                        }
+
+                        // Загружаем значение переключателя из базы данных Firebase
+
+                        if (snapshot.child("catSwitch").exists()) {
+                            boolean catSwitch = snapshot.child("catSwitch").getValue(Boolean.class);
+                            binding.catSwitch.setChecked(catSwitch);
+                        } else {
+                            // Устанавливаем значение по умолчанию для переключателя
+                            binding.catSwitch.setChecked(false);
+                            // Сохраняем значение по умолчанию в базе данных Firebase
+                            HashMap<String, Object> data = new HashMap<>();
+                            data.put("catSwitch", false);
+                            firebaseDatabase.getReference("Users").child(userId).updateChildren(data);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // Обработать ошибку
+                }
+            });
 
             // Загружаем клики из базы данных Firebase
             DatabaseReference clicksRef = firebaseDatabase.getReference("Users").child(userId).child("clicks");
@@ -106,11 +165,16 @@ public class ClickActivity extends AppCompatActivity {
                 }
                 clicks += clicksToAdd;
 
-                mediaPlayer.start();
+                // Проверяем, включен ли переключатель
+                if (binding.catSwitch.isChecked()) {
+                    // Проигрываем мяуканье
+                    mediaPlayer2.start();
+                } else {
+                    // Проигрываем лай
+                    mediaPlayer.start();
+                }
 
                 imageView.startAnimation(rotateAnimation);
-
-
 
                 if (firebaseUser != null) {
                     HashMap<String, Object> data = new HashMap<>();
@@ -133,44 +197,91 @@ public class ClickActivity extends AppCompatActivity {
         int clicksCount = clicks;
         String clicksText = String.valueOf(clicksCount);
 
-        if (clicksCount >= 3000){
-            imageView.setImageResource(R.drawable.img_resize6);
-            if (clicksCount == 3000 && !isReached[4]){
-                showSnackbar("Так держать, вы добрались до 3000");
-                mediaPlayer1.start();
-                isReached[4] = true;
+        // Проверяем, включен ли переключатель
+        if (binding.catSwitch.isChecked()) {
+            // Изменяем изображение на cat.xml, cat2.xml, cat3.xml и т.д. в зависимости от количества кликов
+            if (clicksCount >= 3000) {
+                imageView.setImageResource(R.drawable.cat6);
+                if (clicksCount == 3000 && !isReached[4]) {
+                    showSnackbar("Так держать, вы добрались до 3000");
+                    mediaPlayer1.start();
+                    isReached[4] = true;
+                }
+            } else if (clicksCount >= 1600) {
+                imageView.setImageResource(R.drawable.cat5);
+                if (clicksCount == 1600 && !isReached[3]) {
+                    showSnackbar("Так держать, вы добрались до 1600");
+                    mediaPlayer1.start();
+                    isReached[3] = true;
+                }
+            } else if (clicksCount >= 1000) {
+                imageView.setImageResource(R.drawable.cat4);
+                if (clicksCount == 1000 && !isReached[2]) {
+                    showSnackbar("Так держать, вы добрались до 1000");
+                    mediaPlayer1.start();
+                    isReached[2] = true;
+                }
+            } else if (clicksCount >= 600) {
+                imageView.setImageResource(R.drawable.cat3);
+                if (clicksCount == 600 && !isReached[1]) {
+                    showSnackbar("Так держать, вы добрались до 600");
+                    mediaPlayer1.start();
+                    isReached[1] = true;
+                }
+            } else if (clicksCount >= 200) {
+                imageView.setImageResource(R.drawable.cat2);
+                if (clicksCount == 200 && !isReached[0]) {
+                    showSnackbar("Так держать, вы добрались до 200");
+                    mediaPlayer1.start();
+                    isReached[0] = true;
+                }
+            } else {
+                imageView.setImageResource(R.drawable.cat);
             }
-        }
-        else if (clicksCount >= 1600) {
-            imageView.setImageResource(R.drawable.img_resize5);
-            if (clicksCount == 1600 && !isReached[3]) {
-                showSnackbar("Так держать, вы добрались до 1600");
-                mediaPlayer1.start();
-                isReached[3] = true;
-            }
-        } else if (clicksCount >= 1000) {
-            imageView.setImageResource(R.drawable.img_resize4);
-            if (clicksCount == 1000 && !isReached[2]) {
-                showSnackbar("Так держать, вы добрались до 1000");
-                mediaPlayer1.start();
-                isReached[2] = true;
-            }
-        } else if (clicksCount >= 600) {
-            imageView.setImageResource(R.drawable.img_resize3);
-            if (clicksCount == 600 && !isReached[1]) {
-                showSnackbar("Так держать, вы добрались до 600");
-                mediaPlayer1.start();
-                isReached[1] = true;
-            }
-        } else if (clicksCount >= 200) {
-            imageView.setImageResource(R.drawable.img_resize2);
-            if (clicksCount == 200 && !isReached[0]) {
-                showSnackbar("Так держать, вы добрались до 200");
-                mediaPlayer1.start();
-                isReached[0] = true;
-            }
+
+            binding.headerTit2.setText("Нажми на кошку!");
         } else {
-            imageView.setImageResource(R.drawable.img_resize);
+            // Изменяем изображение в зависимости от количества кликов
+            if (clicksCount >= 3000) {
+                imageView.setImageResource(R.drawable.img_resize6);
+                if (clicksCount == 3000 && !isReached[4]) {
+                    showSnackbar("Так держать, вы добрались до 3000");
+                    mediaPlayer1.start();
+                    isReached[4] = true;
+                }
+            } else if (clicksCount >= 1600) {
+                imageView.setImageResource(R.drawable.img_resize5);
+                if (clicksCount == 1600 && !isReached[3]) {
+                    showSnackbar("Так держать, вы добрались до 1600");
+                    mediaPlayer1.start();
+                    isReached[3] = true;
+                }
+            } else if (clicksCount >= 1000) {
+                imageView.setImageResource(R.drawable.img_resize4);
+                if (clicksCount == 1000 && !isReached[2]) {
+                    showSnackbar("Так держать, вы добрались до 1000");
+                    mediaPlayer1.start();
+                    isReached[2] = true;
+                }
+            } else if (clicksCount >= 600) {
+                imageView.setImageResource(R.drawable.img_resize3);
+                if (clicksCount == 600 && !isReached[1]) {
+                    showSnackbar("Так держать, вы добрались до 600");
+                    mediaPlayer1.start();
+                    isReached[1] = true;
+                }
+            } else if (clicksCount >= 200) {
+                imageView.setImageResource(R.drawable.img_resize2);
+                if (clicksCount == 200 && !isReached[0]) {
+                    showSnackbar("Так держать, вы добрались до 200");
+                    mediaPlayer1.start();
+                    isReached[0] = true;
+                }
+            } else {
+                imageView.setImageResource(R.drawable.img_resize);
+            }
+
+            binding.headerTit2.setText("Нажми на собаку!");
         }
 
         // Отображаем количество кликов на экране
